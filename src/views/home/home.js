3b0258db07a56d7ad21b76263efd8cbc17a1e369
[SW4/1반] 신혜지 <shinhj4068@gmail.com>
2023-04-20 23:16:29 +0900

    comment: home.js 주석 추가

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -6,8 +6,10 @@ const categoryWrap = document.querySelector("#category");
 const width = getImageWidth();
 let currentSlide = 0;
 
+// slide 5초씩 반복
 setInterval(slideImage, 5000);
 
+// slide 할 image width 구하기
 function getImageWidth() {
   const img = new Image();
   
@@ -15,6 +17,7 @@ function getImageWidth() {
   return img.width;
 }
 
+// slide animation
 function slideImage() {
   let from = -(width * currentSlide);
   let to = from - width;   
@@ -35,45 +38,53 @@ function slideImage() {
   }
 }
 
-const categoryList = ['웹 개발', '프론트엔드', '백엔드', '모바일 앱 개발', '게임 개발', '알고리즘·자료구조', '데이터베이스']
+// 임시 카테고리 데이터
+const categoryList = ['웹 개발', '프론트엔드', '백엔드', '모바일 앱 개발', '게임 개발', '알고리즘·자료구조', '데이터베이스'];
+
+// 해당 카테고리 책 권수 
 const productCounter = document.getElementById("product-counter");
 
+// 카테고리 html 생성
 const createCategory = (category) => {
   return `<li class="nav-item category">
             <a class="nav-link clicked" href="#">${category}</a>
           </li>`
 }
 
+// 카테고리 html 추가
 categoryList.forEach(category => {
   const categoryEl = createCategory(category);
   categoryWrap.innerHTML += categoryEl;
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const navLinks = document.querySelectorAll('.nav-link');
+// 페이지가 로드되면 실행
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".nav-link");
   let isFirst = true;
 
+  // 카테고리 누르면 해당 카테고리로 선택
   navLinks.forEach(link => {
-    link.addEventListener('click', (event) => {
+    link.addEventListener("click", (event) => {
       event.preventDefault();
 
-      const clickedLink = document.querySelector('.nav-link.clicked');
+      const clickedLink = document.querySelector(".nav-link.clicked");
       if (clickedLink) {
-        clickedLink.classList.remove('clicked');
+        clickedLink.classList.remove("clicked");
       }
 
-      link.classList.add('clicked');
+      link.classList.add("clicked");
       productCounter.textContent = `${event.target.textContent} (0)`;
     });
   });
 
+  // 처음 실행되면 카테고리 중 전체가 선택 됨
   navLinks.forEach((link) => {
     if (isFirst) {
-      link.classList.add('clicked');
+      link.classList.add("clicked");
       productCounter.textContent = `전체 (0)`;
       isFirst = false;
     } else {
-      link.classList.remove('clicked');
+      link.classList.remove("clicked");
     }
   });
 });
