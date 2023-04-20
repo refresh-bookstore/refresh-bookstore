16e0670274c5a134bb094506298e3344f34daa76
youngchae <010101chae@gmail.com>
2023-04-21 04:30:36 +0900

    design: 체크박스 사이즈 변경

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -4,10 +4,13 @@ const bookTitle = document.querySelectorAll('.book-title');
 const author = document.querySelectorAll('.author')
 const bookPrice = document.querySelectorAll('.book-price');
 
-const minusBtn = document.querySelector('#minusBtn');
-const plusBtn = document.querySelector('#plusBtn');
-const amountInput = document.querySelector('#amountInput');
-const totalCost = document.querySelector('#totalCost');
+const minusBtn = document.querySelectorAll('.minusBtn');
+const amountInput = document.querySelectorAll('.amountInput');
+const plusBtn = document.querySelectorAll('.plusBtn');
+
+const selectedPrice = document.querySelector('.selectedPrice');
+const deliveryFee = document.querySelector('.deliveryFee');
+const totalCost = document.querySelector('.totalCost');
 
 //예시데이터
 const book = {
@@ -21,6 +24,11 @@ const book = {
   isbn: '1162245557',
   introduction: `혼자 해도 충분합니다! 1:1 과외하듯 배우는 IT 지식 입문서! 이 책은 독학으로 IT 지식을 배우는 입문자가 ‘꼭 필요한 내용을 제대로 학습’할 수 있도록 구성했다. 뭘 모르는지조차 모르는 입문자의 막연한 마음에 십분 공감하여 과외 선생님이 알려주듯 친절하게, 핵심 내용만 콕콕 집어 준다. 1장에서는 IT 업계 용어를 알아보며 개발과 개발자를 이해하고, 2장에서는 개발자가 실제로 사용하는 용어를 배우며 개발자와 소통할 수 있는 발판을 마련해준다. 마지막 3장에서는 여러 가지 개발 용어를 바탕으로 개발자의 길로 들어설 수 있도록 친절하게 알려 준다. '개발이 뭔지 궁금했지만', '개발자와 소통해야 하지만', '개발자가 되고 싶지만'기존 IT 지식서에서는 시원하게 알 수 없었던 진짜 코딩 지식을 [혼공 얄코]에서 만나 보자!`,
 };
+
+// 책 정보
+bookTitle.forEach(e => e.innerText = book.title);
+author.forEach(e => e.innerText = book.author);
+bookPrice.forEach(e => e.innerText = `${book.cost.toLocaleString()}원`);
 
 
 // 체크박스 구현
@@ -49,23 +57,22 @@ function checkAll(selectAll) {
 selectAll.addEventListener('click', () => checkAll(selectAll));
 
 
-// 수량 변경
-minusBtn.addEventListener('click',(e) => {
-  e.preventDefault();
-  if (Number(amountInput.value) <= 1) {
+// 수량 변경 + 합계 자동 계산
+// minusBtn.forEach(btn => btn.addEventListener('click',(e) => {
+//   e.preventDefault();
+//   if (Number(amountInput.value) <= 1) {
 
-  } else {
-    amountInput.value -= 1;
-    totalCost.innerText = `${(book.cost * amountInput.value).toLocaleString()}원`;
-  }
-  
-});
+//   } else {
+//     amountInput.value -= 1;
+//     totalCost.innerText = `${(book.cost * amountInput.value).toLocaleString()}원`;
+//   }
+// }));
 
-plusBtn.addEventListener('click',(e)=>{
-  e.preventDefault();
-  amountInput.value = Number(amountInput.value) + 1;
-  totalCost.innerText = `${(book.cost * amountInput.value).toLocaleString()}원`;
-});
+// plusBtn.forEach(btn => btn.addEventListener('click',(e)=>{
+//   e.preventDefault();
+//   amountInput.value = Number(amountInput.value) + 1;
+//   totalCost.innerText = `${(book.cost * amountInput.value).toLocaleString()}원`;
+// }));
 
 // 삭제 구현
 // 단일 상품 삭제
@@ -78,11 +85,13 @@ deleteBtn.forEach(btn => {
 // 선택 삭제
 const deleteSelectedBtn = document.querySelector('.deleteSelected');
 deleteSelectedBtn.addEventListener('click', () => {
-  console.log(checkboxes);
-  const selectedItem = 
   checkboxes.forEach(box => {
     if (box.checked) box.parentElement.setAttribute('style', 'display: none');
   });
 });
+
+// 가격 계산 구현
+// 선택 상품 금액 계산
+// 
 
 main();
