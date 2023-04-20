55d2d9e15e1f105c83dd516ab6827a8d4e83a83f
[SW4/1반] 신혜지 <shinhj4068@gmail.com>
2023-04-21 03:17:28 +0900

    feat: 카테고리별 책 상품 보여주기 구현

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,7 +1,6 @@
 const slider = document.querySelector("#slider");
 const slides = slider.querySelector(".slides");
 const slide = slides.querySelectorAll(".slide");
-const categoryWrap = document.querySelector("#category");
 
 const width = getImageWidth();
 let currentSlide = 0;
@@ -51,19 +50,103 @@ const createCategory = (category) => {
           </li>`
 }
 
+const categoryWrap = document.querySelector("#category");
+
 // 카테고리 html 추가
 categoryList.forEach(category => {
   const categoryEl = createCategory(category);
   categoryWrap.innerHTML += categoryEl;
 });
 
+const books = document.querySelector(".books");
+const bookTitle = document.querySelector(".book-text.title");
+const bookCategory = document.querySelector(".book-text.category");
+const bookPrice = document.querySelector(".book-text.price");
+
+// 책 html 생성
+// href는 예시: /books/${book._id}
+const createBook = (book) => {
+  return `<div class="book">
+            <a class="book-link" href="#">
+              <div class="img-container">
+                <img src="../../../public/images/img_sample_book.png" class="book-img" alt="${book.title}"/>
+              </div>
+              <div class="book-body">
+                <div class="book-text title">${book.title}</div>
+                <div class="book-text category">${book.category}</div>
+                <div class="book-text price">${book.price}</div>
+              </div>
+            </a>
+          </div>`;
+};
+
+// 임시 책 데이터
+const bookList = [{
+  title: "프론트엔드 성능 최적화 가이드",
+  category: categoryList[1],
+  price: "19800원"
+}, {
+  title: "Hello IT 프론트엔드 개발을 시작하려고 해: 입문편",
+  category: categoryList[1],
+  price: "26550원"
+}, {
+  title: "Node.js 백엔드 개발자 되기",
+  category: categoryList[2],
+  price: "34200원"
+}, {
+  title: "백엔드를 위한 Django REST Framework with 파이썬",
+  category: categoryList[2],
+  price: "16200원"
+}, {
+  title: "인사이드 안드로이드 OS",
+  category: categoryList[3],
+  price: "25200원"
+}, {
+  title: "iOS 앱 개발을 위한 Swift 3",
+  category: categoryList[3],
+  price: "38700원"
+}, {
+  title: "유니티 2D 게임 개발",
+  category: categoryList[4],
+  price: "27000원"
+}, {
+  title: "Do it! 첫 알고리즘",
+  category: categoryList[5],
+  price: "16200원"
+}, {
+  title: "누구나 자료 구조와 알고리즘",
+  category: categoryList[5],
+  price: "29700원"
+}, {
+  title: "데이터베이스 개론",
+  category: categoryList[6],
+  price: "29000원"
+}, ];
+
+// 책 html 추가
+bookList.forEach(book => {
+  const bookEl = createBook(book);
+  books.innerHTML += bookEl;
+});
+
+const categoryLiList = document.querySelectorAll(".nav-item.category");
+
 // 페이지가 로드되면 실행
 document.addEventListener("DOMContentLoaded", () => {
   const navLinks = document.querySelectorAll(".nav-link");
   let isFirst = true;
 
-  // 카테고리 누르면 해당 카테고리로 선택
   navLinks.forEach(link => {
+    // 페이지가 로드 됐을 때 전체 카테고리로 보여줌
+    if (isFirst) {
+      link.classList.add("clicked");
+      productCounter.textContent = `전체 (${bookList.length})`;
+      isFirst = false;
+    } else {
+      link.classList.remove("clicked");
+    }
+
+    // 카테고리 누르면 해당 카테고리로 선택
     link.addEventListener("click", (event) => {
       event.preventDefault();
 
@@ -71,20 +154,36 @@ document.addEventListener("DOMContentLoaded", () => {
       if (clickedLink) {
         clickedLink.classList.remove("clicked");
       }
-
       link.classList.add("clicked");
-      productCounter.textContent = `${event.target.textContent} (0)`;
+
+      // 책 초기화
+      books.innerHTML = "";
+
+      const findBookListByCategory = [];
+
+      const clickedCategory = event.target.textContent;
+
+      // 클릭한 카테고리별 책 배열에 담기
+      bookList.forEach((book) => {
+        if (book.category.includes(clickedCategory) || clickedCategory === "전체") {
+          findBookListByCategory.push(book);
+        }
+      });
+
+      productCounter.textContent = `${clickedCategory} (${findBookListByCategory.length})`;
+
+      if (findBookListByCategory.length === 0) {
+        books.classList.add("empty");
+        books.innerHTML = `<div></div>
+                          <div id="empty-book-list">상품이 없습니다</div>`;
+      } else {
+        books.classList.remove("empty");
+        findBookListByCategory.forEach(book => {
+          const newBook = createBook(book);
+          books.innerHTML += newBook;
+        });
+        
+      }
     });
-  });
-
-  // 처음 실행되면 카테고리 중 전체가 선택 됨
-  navLinks.forEach((link) => {
-    if (isFirst) {
-      link.classList.add("clicked");
-      productCounter.textContent = `전체 (0)`;
-      isFirst = false;
-    } else {
-      link.classList.remove("clicked");
-    }
   });
 });
