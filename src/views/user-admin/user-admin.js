241a989cda436a65f33c6ae05b161ad34a4a9a8c
baansuk <baansuk_hwang@naver.com>
2023-04-22 12:06:05 +0900

    feat: 관리자페이지 목록 조회 기능 추가

--- src/views/user-admin/user-admin.js
+++ src/views/user-admin/user-admin.js
@@ -28,6 +28,8 @@ const contents = [
   adminContentCategories,
 ];
 
+
+//////////////////////목록별 페이지 구현///////////////////////
 lists.forEach((e)=> {
   e.addEventListener('click', ()=>{
     contents.map((el) => {
@@ -39,23 +41,105 @@ lists.forEach((e)=> {
     })
     title.innerText = e.innerText;
 
+
+    //////////////////////도서관리///////////////////////
     if(lists.indexOf(e) === 0){
-      for(let i = 0; i < dataLists.length; i++){
-        const item = document.createElement('div').classList.add('item');
-        item.innerHTML=`
-        <div class="item-img">
-        <img src="../public/images/sample_image.jpg">
+      contents[0].innerHTML = ""; 
+      for(let i = 0; i < 5; i++){
+        contents[0].innerHTML += 
+        `
+        <div class="admin-items">
+            <input type="checkbox">
+            <div class="item-info">
+              <p class="item-added-date"> 2022-05-14</p>
+              <div class="item-more-info">
+                <p class="item-name"> 혼자 공부하는 얄팍한 코딩지식</p>
+                <p class="item-detail"> 한빛 출판사 | 고현민 | 2022</p>
+              </div>
+              <p class="item-cost"> 15,800원</p>
+              <span class="admin-buttons">
+                <img class="button" src="../public/images/icon_edit.svg">
+                <img class="button red" src="../public/images/icon_delete.svg">
+              </span> 
+            </div>
+
+        `
+      }
+    }
+
+    //////////////////////회원관리///////////////////////
+    if(lists.indexOf(e) === 1){
+      contents[1].innerHTML = ""; 
+      for(let i = 0; i < 5; i++){
+        contents[1].innerHTML += 
+        `
+        <div class="admin-items">
+          <input type="checkbox">
+          <div class="item-info">
+            <p class="item-added-date"> 2022-05-14</p>
+            <div class="item-more-info">
+              <p class="item-name"> 김토끼</p>
+              <p class="item-detail"> elice@rabbit.com </p>
+            </div>
+            <span class="user-buttons">
+              <img class="button set-user-admin" title="관리자 등록" src="../public/images/icon_user_admin.svg">
+              <img class="button edit-user" title="회원 정보 수정" src="../public/images/icon_user_edit.svg">
+              <img class="button remove-user" title="회원 탈퇴" src="../public/images/icon_user_remove.svg">
+            </span>
+          </div>
         </div>
-        <div class="item-info">
-          <p class="item-name"> 혼자 공부하는 얄팍한 코딩지식</p>
-          <p class="item-detail"> 한빛 출판사 | 고현민 | 2022</p>
-          <p class="item-cost"> 15,800원</p>
-          <span class="item-buttons">
-            <button>수정</button>
-            <button class="red">삭제</button>
-        </span> 
-        </div>`
-        contents[0].appendChild(item);
+        `
+      }
+    }
+
+    //////////////////////주문관리///////////////////////
+    if(lists.indexOf(e) === 2){
+      contents[2].innerHTML = ""; 
+      for(let i = 0; i < 5; i++){
+        contents[2].innerHTML += 
+        `
+        <div class="admin-items ordered-list">
+          <div class="order-upper">
+              <input type="checkbox">
+              <p class="item-name order"> 230421001 </p>
+              <p class="order-status"> 배송중</p>
+          </div>
+          <div class="order-lower">
+            <div class="item-info order">
+              <div class="item-more-info order a">
+                <p class="item-detail order"> 김토끼</p>
+                <p class="item-detail order"> 010-2323-2323</p>
+                <p class="item-detail order"> 서울시 양천구 오목로359 사무실</p>
+              </div>
+              <div class="item-more-info order b">
+                <p class="item-detail order"> 혼자 공부하는 얄팍한 코딩 지식 (3)</p>
+                <p class="item-detail order"> 코딩 관련 책 이름 하나</p>
+                <p class="item-detail order"> 이것도 코딩에 관련된 책</p>
+                <p class="item-detail order"> 책 이름은 모르지만 코딩임(2)</p>
+              </div>
+              <p class="item-cost"> 23,400원</p>
+              <span class="admin-buttons">
+                <img class="button" title="수정" src="../public/images/icon_edit.svg">
+                <img class="button red" title="삭제" src="../public/images/icon_delete.svg">
+              </span>
+            </div>
+          </div>
+        </div>
+        `
+      }
+    }
+
+    //////////////////////카테고리관리///////////////////////
+    if(lists.indexOf(e) === 3){
+      contents[3].innerHTML = ""; 
+      for(let i = 0; i < 5; i++){
+        contents[3].innerHTML += 
+        `
+        <div class="category-box">
+            <p class="category-name">프론트엔드</p>
+            <img class="button red" title="삭제" src="../public/images/icon_delete.svg">
+        </div>
+        `
       }
     }
 
