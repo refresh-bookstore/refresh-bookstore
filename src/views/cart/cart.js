b93c4534911c019cb68dc434689bdf2160e27643
youngchae <010101chae@gmail.com>
2023-04-24 01:03:35 +0900

    feat: book-detail로부터 넘어오는 로컬스토리지 데이터 이용 코드 구현

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -5,61 +5,63 @@ function getPriceNumber(str) {
   return Number(str.replace(/,/g, '').slice(0, -1));
 }
 
+// 로컬스토리지에 'cart'데이터 저장(테스트)
+const save = (data) => {
+  localStorage.setItem('cart', JSON.stringify(data));
+}
+
 const cart = document.querySelector('.cart');
 const selectedPrice = document.querySelector('.selectedPrice');
 const deliveryFee = document.querySelector('.deliveryFee');
 const totalCost = document.querySelector('.totalCost');
 
 // 예시 주문데이터
-const order = [
-  {
-    "title":"프론트엔드 성능 최적화 가이드",
-    "author":"유동균",
-    "publisher":"인사이트",
-    "publication_date":"2022-11-14T15:00:00.000Z",
-    "isbn":"9788966263745",
-    "description":"'웹 성능 최적화'는 프론트엔드 개발자라면 반드시 고민해야 하는 이슈다. 서비스 환경과 상황에 따라 필요한 최적화 포인트가 다르고, 기법도 매우 다양하기에 개발 중인 서비스 특성에 맞게 커스터마이징하는 능력이 중요하다.\n\n  저자는 수많은 기업의 웹 서비스 성능을 컨설팅하고, 삼성 SSAFY, 프로그래머스, 인프런, 스터디파이 등 강의 플랫폼에서 최적화 및 개발 강의를 진행한 경험을 책에 고스란히 녹여 냈다. '성능 최적화' 주제를 효과적으로 배우는 데 이론보다는 실습이 중요함을 강조하며 현장감 있게 구성했다. 이 책은 실생활에서 흔히 개발하는 4가지 실습 사이트를 예제로, 직관적인 최적화 노하우를 전달한다.",
-    "price":22000,
-    "image_path":"../product-images/9788966263745.png",
-    "category":"프론트엔드",
-    "amount":1,
-  },
-  {
-    "title":"이펙티브 타입스크립트",
-    "author":"댄 밴더캄",
-    "publisher":"인사이트",
-    "publication_date":"2021-06-08T15:00:00.000Z",
-    "isbn":"9788966263134",
-    "description":"타입스크립트는 타입 정보를 지닌 자바스크립트의 상위 집합으로, 자바스크립트의 골치 아픈 문제점들을 해결해 준다. 이 책은 《이펙티브 C++》와 《이펙티브 자바》의 형식을 차용해 타입스크립트의 동작 원리, 해야 할 것과 하지 말아야 할 것에 대한 구체적인 조언을 62가지 항목으로 나누어 담았다. 각 항목의 조언을 실제로 적용한 예제를 통해 연습하다 보면 타입스크립트를 효율적으로 사용하는 방법을 익힐 수 있다.",
-    "price":25000,
-    "image_path":"../product-images/9788966263134.png",
-    "category":"프론트엔드",
-    "amount":2,
-  }
-];
+// const order = [
+//   {
+//     "title":"프론트엔드 성능 최적화 가이드",
+//     "author":"유동균",
+//     "publisher":"인사이트",
+//     "publication_date":"2022-11-14T15:00:00.000Z",
+//     "isbn":"9788966263745",
+//     "description":"'웹 성능 최적화'는 프론트엔드 개발자라면 반드시 고민해야 하는 이슈다. 서비스 환경과 상황에 따라 필요한 최적화 포인트가 다르고, 기법도 매우 다양하기에 개발 중인 서비스 특성에 맞게 커스터마이징하는 능력이 중요하다.\n\n  저자는 수많은 기업의 웹 서비스 성능을 컨설팅하고, 삼성 SSAFY, 프로그래머스, 인프런, 스터디파이 등 강의 플랫폼에서 최적화 및 개발 강의를 진행한 경험을 책에 고스란히 녹여 냈다. '성능 최적화' 주제를 효과적으로 배우는 데 이론보다는 실습이 중요함을 강조하며 현장감 있게 구성했다. 이 책은 실생활에서 흔히 개발하는 4가지 실습 사이트를 예제로, 직관적인 최적화 노하우를 전달한다.",
+//     "price":22000,
+//     "image_path":"../product-images/9788966263745.png",
+//     "category":"프론트엔드",
+//     "amount":1,
+//   },
+//   {
+//     "title":"이펙티브 타입스크립트",
+//     "author":"댄 밴더캄",
+//     "publisher":"인사이트",
+//     "publication_date":"2021-06-08T15:00:00.000Z",
+//     "isbn":"9788966263134",
+//     "description":"타입스크립트는 타입 정보를 지닌 자바스크립트의 상위 집합으로, 자바스크립트의 골치 아픈 문제점들을 해결해 준다. 이 책은 《이펙티브 C++》와 《이펙티브 자바》의 형식을 차용해 타입스크립트의 동작 원리, 해야 할 것과 하지 말아야 할 것에 대한 구체적인 조언을 62가지 항목으로 나누어 담았다. 각 항목의 조언을 실제로 적용한 예제를 통해 연습하다 보면 타입스크립트를 효율적으로 사용하는 방법을 익힐 수 있다.",
+//     "price":25000,
+//     "image_path":"../product-images/9788966263134.png",
+//     "category":"프론트엔드",
+//     "amount":2,
+//   }
+// ];
 
-// 로컬스토리지에 'cart'데이터 저장(테스트)
-const save = (data) => {
-  localStorage.setItem('cart', JSON.stringify(data));
-}
-const bookData = {
-  "title":"모던 자바스크립트 Deep Dive ",
-  "author":"이웅모",
-  "publisher":"위키북스",
-  "publication_date":"2020-09-24T15:00:00.000Z",
-  "isbn":"9791158392239",
-  "description":"자바스크립트를 둘러싼 기본 개념을 정확하고 구체적으로 설명하고, 자바스크립트 코드의 동작 원리를 집요하게 파헤친다. 작성한 코드가 컴퓨터 내부에서 어떻게 동작할 것인지 예측하고, 명확히 설명할 수 있도록 돕는다. 또한 최신 자바스크립트 명세를 반영해 안정적이고 효율적인 코드를 작성할 수 있는 기본기를 다지고, 실전에서 쓰이는 모던 자바스크립트 프레임워크나 도구를 완벽하게 이해하고 활용할 수 있게 도와준다.",
-  "price":45000,
-  "image_path":"../product-images/9791158392239.png",
-  "category":"프론트엔드",
-  "amount":1,
-};
-order.push(bookData);
-save(order);
+// const bookData = {
+//   "title":"모던 자바스크립트 Deep Dive ",
+//   "author":"이웅모",
+//   "publisher":"위키북스",
+//   "publication_date":"2020-09-24T15:00:00.000Z",
+//   "isbn":"9791158392239",
+//   "description":"자바스크립트를 둘러싼 기본 개념을 정확하고 구체적으로 설명하고, 자바스크립트 코드의 동작 원리를 집요하게 파헤친다. 작성한 코드가 컴퓨터 내부에서 어떻게 동작할 것인지 예측하고, 명확히 설명할 수 있도록 돕는다. 또한 최신 자바스크립트 명세를 반영해 안정적이고 효율적인 코드를 작성할 수 있는 기본기를 다지고, 실전에서 쓰이는 모던 자바스크립트 프레임워크나 도구를 완벽하게 이해하고 활용할 수 있게 도와준다.",
+//   "price":45000,
+//   "image_path":"../product-images/9791158392239.png",
+//   "category":"프론트엔드",
+//   "amount":1,
+// };
+// order.push(bookData);
+// save(order);
 
+let data = JSON.parse(localStorage.getItem('cart'));
 // 로컬스토리지에서 데이터 불러오기, 체크박스, 수량조절, 삭제버튼 활성화, 가격 출력 자동화
 function renderBooks() {
-  const data = JSON.parse(localStorage.getItem('cart'));
+  data = JSON.parse(localStorage.getItem('cart'));
   if (data.length === 0) {
     // 장바구니에 상품이 없습니다
     cart.innerHTML = `<p class="empty-cart">상품이 없습니다.</p>`;
@@ -69,7 +71,7 @@ function renderBooks() {
       const imgLink = order.image_path;
       const title = order.title;
       const author = order.author;
-      const amount = Number(order.amount);
+      const amount = order.amount;
       const bookPrice = `${(order.price * amount).toLocaleString()}원`;
       
       cart.innerHTML += 
@@ -103,11 +105,11 @@ function renderBooks() {
       </div>
       </div>`;
     });
+  }
     activateCheckboxes();
     activateAmountBtn();
     activateDeleteBtn();
     setPayList();
-  }
 }
 renderBooks();
 
@@ -149,11 +151,11 @@ function activateAmountBtn() {
   plusBtn.forEach((btn, idx) => {
     btn.addEventListener('click', () => {
       btn.previousElementSibling.value = Number(btn.previousElementSibling.value) + 1;
-      order[idx].amount += 1;
+      data[idx].amount += 1;
       localStorage.removeItem('cart');
-      save(order);
+      save(data);
       const bookPrice = document.querySelector(`.book-price${idx}`);
-      bookPrice.innerText = `${(order[idx].amount * order[idx].price).toLocaleString()}원`;
+      bookPrice.innerText = `${(data[idx].amount * data[idx].price).toLocaleString()}원`;
       setPayList();
     });
   });  
@@ -164,11 +166,11 @@ function activateAmountBtn() {
         btn.nextElementSibling.value = 1;
       } else {
         btn.nextElementSibling.value = Number(btn.nextElementSibling.value) - 1;
-        order[idx].amount -= 1;
+        data[idx].amount -= 1;
         localStorage.removeItem('cart');
-        save(order);
+        save(data);
         const bookPrice = document.querySelector(`.book-price${idx}`);
-        bookPrice.innerText = `${(order[idx].amount * order[idx].price).toLocaleString()}원`;
+        bookPrice.innerText = `${(data[idx].amount * data[idx].price).toLocaleString()}원`;
       }
       setPayList();
     });
@@ -180,11 +182,11 @@ function activateAmountBtn() {
       if (amount.value <= 1 || amount.value === undefined) {
         amount.value = 1;
       } 
-      order[idx].amount = Number(amount.value);
+      data[idx].amount = Number(amount.value);
       localStorage.removeItem('cart');
-      save(order);
+      save(data);
       const bookPrice = document.querySelector(`.book-price${idx}`);
-      bookPrice.innerText = `${(order[idx].amount * order[idx].price).toLocaleString()}원`;
+      bookPrice.innerText = `${(data[idx].amount * data[idx].price).toLocaleString()}원`;
       setPayList();
     });
   });
@@ -195,10 +197,9 @@ function activateDeleteBtn() {
   const deleteBtn = document.querySelectorAll('.deleteBtn');
   deleteBtn.forEach((btn, idx) => {
     btn.addEventListener('click', () => {
-      order.splice(idx, 1);
+      data.splice(idx, 1);
       localStorage.removeItem('cart');
-      save(order);
-      cart.innerHTML = '';
+      save(data);
       renderBooks();
     });
   });
@@ -217,10 +218,10 @@ function activateDeleteSelectedBtn() {
     }
     if (checkedIndex.length) {
       checkedIndex.forEach(indexNum => {
-        order.splice(indexNum, 1);
+        data.splice(indexNum, 1);
       });
       localStorage.removeItem('cart');
-      save(order);
+      save(data);
       cart.innerHTML = '';
       renderBooks();
     }
@@ -239,7 +240,6 @@ function setPayList() {
 // 선택 상품 금액 계산 함수
 function setSelectedPrice() {
   const items = document.querySelectorAll('.item input[type="checkbox"]');
-  console.log(items);
   let bookPriceSum = 0;
   if (items.length) {
     items.forEach((item, idx) => {
