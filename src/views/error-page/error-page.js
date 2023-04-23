7deefdfcdf00a04a40c14b15545cd0f9e97e1d4b
[SW4/1반] 신혜지 <shinhj4068@gmail.com>
2023-04-23 20:42:44 +0900

    style: error-page.css 선택자 class로 통일

--- src/views/error-page/error-page.js
+++ src/views/error-page/error-page.js
@@ -1,6 +1,6 @@
 import { main } from '../public/js/main.js';
 
-const button = document.getElementById("button");
+const button = document.getElementById("return-home-button");
 
 // 홈으로 돌아가기
 button.addEventListener("click", () => {
