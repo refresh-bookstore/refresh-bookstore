10b45d623d1d1a60b13f077498a81e4093617bec
youngchae <010101chae@gmail.com>
2023-04-23 16:50:28 +0900

    design: 이미지 사이즈 조절, 이미지 테두리 생성

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -12,28 +12,28 @@ const totalCost = document.querySelector('.totalCost');
 // 예시 주문데이터
 let order = [
   {
-    title: '혼자 공부하는 얄팍한 코딩지식',
-    subtitle: '비전공자도 1:1 과외하듯 배우는 IT 지식 입문서',
-    author: '고현민',
-    publisher: '한빛출판사',
-    published: new Date(2022, 4, 25),
-    cost: 14800,
-    category: '코딩입문서',
-    isbn: '9791162245552',
-    introduction: `혼자 해도 충분합니다! 1:1 과외하듯 배우는 IT 지식 입문서! 이 책은 독학으로 IT 지식을 배우는 입문자가 ‘꼭 필요한 내용을 제대로 학습’할 수 있도록 구성했다. 뭘 모르는지조차 모르는 입문자의 막연한 마음에 십분 공감하여 과외 선생님이 알려주듯 친절하게, 핵심 내용만 콕콕 집어 준다. 1장에서는 IT 업계 용어를 알아보며 개발과 개발자를 이해하고, 2장에서는 개발자가 실제로 사용하는 용어를 배우며 개발자와 소통할 수 있는 발판을 마련해준다. 마지막 3장에서는 여러 가지 개발 용어를 바탕으로 개발자의 길로 들어설 수 있도록 친절하게 알려 준다. '개발이 뭔지 궁금했지만', '개발자와 소통해야 하지만', '개발자가 되고 싶지만'기존 IT 지식서에서는 시원하게 알 수 없었던 진짜 코딩 지식을 [혼공 얄코]에서 만나 보자!`,
-    amount: 1
+    "title":"프론트엔드 성능 최적화 가이드",
+    "author":"유동균",
+    "publisher":"인사이트",
+    "publication_date":"2022-11-14T15:00:00.000Z",
+    "isbn":"9788966263745",
+    "description":"'웹 성능 최적화'는 프론트엔드 개발자라면 반드시 고민해야 하는 이슈다. 서비스 환경과 상황에 따라 필요한 최적화 포인트가 다르고, 기법도 매우 다양하기에 개발 중인 서비스 특성에 맞게 커스터마이징하는 능력이 중요하다.\n\n  저자는 수많은 기업의 웹 서비스 성능을 컨설팅하고, 삼성 SSAFY, 프로그래머스, 인프런, 스터디파이 등 강의 플랫폼에서 최적화 및 개발 강의를 진행한 경험을 책에 고스란히 녹여 냈다. '성능 최적화' 주제를 효과적으로 배우는 데 이론보다는 실습이 중요함을 강조하며 현장감 있게 구성했다. 이 책은 실생활에서 흔히 개발하는 4가지 실습 사이트를 예제로, 직관적인 최적화 노하우를 전달한다.",
+    "price":22000,
+    "image_path":"../product-images/9788966263745.png",
+    "category":"프론트엔드",
+    "amount":1,
   },
   {
-    title: '모던 자바스크립트 Deep Dive',
-    subtitle: '자바스크립트의 기본 개념과 동작 원리',
-    author: '이웅모',
-    publisher: '위키북스',
-    published: new Date(2020, 9, 25),
-    cost: 45000,
-    category: '웹 개발',
-    isbn: '9791158392239',
-    introduction: `『모던 자바스크립트 Deep Dive』에서는 자바스크립트를 둘러싼 기본 개념을 정확하고 구체적으로 설명하고, 자바스크립트 코드의 동작 원리를 집요하게 파헤친다. 따라서 여러분이 작성한 코드가 컴퓨터 내부에서 어떻게 동작할 것인지 예측하고, 명확히 설명할 수 있도록 돕는다. 또한 최신 자바스크립트 명세를 반영해 안정적이고 효율적인 코드를 작성할 수 있는 기본기를 다지고, 실전에서 쓰이는 모던 자바스크립트 프레임워크나 도구를 완벽하게 이해하고 활용할 수 있게 도와준다.`,
-    amount: 1
+    "title":"이펙티브 타입스크립트",
+    "author":"댄 밴더캄",
+    "publisher":"인사이트",
+    "publication_date":"2021-06-08T15:00:00.000Z",
+    "isbn":"9788966263134",
+    "description":"타입스크립트는 타입 정보를 지닌 자바스크립트의 상위 집합으로, 자바스크립트의 골치 아픈 문제점들을 해결해 준다. 이 책은 《이펙티브 C++》와 《이펙티브 자바》의 형식을 차용해 타입스크립트의 동작 원리, 해야 할 것과 하지 말아야 할 것에 대한 구체적인 조언을 62가지 항목으로 나누어 담았다. 각 항목의 조언을 실제로 적용한 예제를 통해 연습하다 보면 타입스크립트를 효율적으로 사용하는 방법을 익힐 수 있다.",
+    "price":25000,
+    "image_path":"../product-images/9788966263134.png",
+    "category":"프론트엔드",
+    "amount":2,
   }
 ];
 console.log(JSON.stringify(order));
@@ -44,16 +44,16 @@ const save = (data) => {
 save(order);
 
 let bookData = {
-  title: '모던 자바스크립트 Deep Dive',
-  subtitle: '자바스크립트의 기본 개념과 동작 원리',
-  author: '이웅모',
-  publisher: '위키북스',
-  published: new Date(2020, 9, 25),
-  cost: 45000,
-  category: '웹 개발',
-  isbn: '9791158392239',
-  introduction: `『모던 자바스크립트 Deep Dive』에서는 자바스크립트를 둘러싼 기본 개념을 정확하고 구체적으로 설명하고, 자바스크립트 코드의 동작 원리를 집요하게 파헤친다. 따라서 여러분이 작성한 코드가 컴퓨터 내부에서 어떻게 동작할 것인지 예측하고, 명확히 설명할 수 있도록 돕는다. 또한 최신 자바스크립트 명세를 반영해 안정적이고 효율적인 코드를 작성할 수 있는 기본기를 다지고, 실전에서 쓰이는 모던 자바스크립트 프레임워크나 도구를 완벽하게 이해하고 활용할 수 있게 도와준다.`,
-  amount: 1
+  "title":"모던 자바스크립트 Deep Dive ",
+  "author":"이웅모",
+  "publisher":"위키북스",
+  "publication_date":"2020-09-24T15:00:00.000Z",
+  "isbn":"9791158392239",
+  "description":"자바스크립트를 둘러싼 기본 개념을 정확하고 구체적으로 설명하고, 자바스크립트 코드의 동작 원리를 집요하게 파헤친다. 작성한 코드가 컴퓨터 내부에서 어떻게 동작할 것인지 예측하고, 명확히 설명할 수 있도록 돕는다. 또한 최신 자바스크립트 명세를 반영해 안정적이고 효율적인 코드를 작성할 수 있는 기본기를 다지고, 실전에서 쓰이는 모던 자바스크립트 프레임워크나 도구를 완벽하게 이해하고 활용할 수 있게 도와준다.",
+  "price":45000,
+  "image_path":"../product-images/9791158392239.png",
+  "category":"프론트엔드",
+  "amount":1,
 };
 order.push(bookData);
 save(order);
@@ -70,12 +70,11 @@ const load = () => {
 };
 
 load().forEach(order => {
-  // imgLink는 isbn.png로 넘길 예정
-  const imgLink = '../public/images/sample_image.jpg';
+  const imgLink = order.image_path;
   const title = order.title;
   const author = order.author;
   const amount = Number(order.amount);
-  const bookPrice = `${(order.cost * amount).toLocaleString()}원`;
+  const bookPrice = `${(order.price * amount).toLocaleString()}원`;
 
   cart.innerHTML += `<div class="item">
   <input type="checkbox" name="buy" checked="">
