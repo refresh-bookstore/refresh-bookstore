c8b7c0bbc4bc8b2abfe559b7b4dc985fd72f512a
[SW4/1반] 신혜지 <shinhj4068@gmail.com>
2023-04-24 17:08:03 +0900

    feat: 회원가입 api 구현

--- src/views/register/register.js
+++ src/views/register/register.js
@@ -4,13 +4,42 @@ import { checkValid } from './checkValid.js';
 const submitButton = document.getElementById("submitButton");
 
 submitButton.addEventListener("click", handleSubmit);
-function handleSubmit(e) {
+async function handleSubmit(e) {
   e.preventDefault();
   const isAllValid = checkValid();
 
   if (isAllValid) {
-    console.log('회원가입 성공');
-    location.replace('../home/home.html');
+    await fetch("/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: nameInput.value,
+        email: emailInput.value,
+        password: passwordInput.value,
+        passwordCheck: passwordCheckInput.value,
+        postalCode: postalCodeInput.value,
+        address: addressInput.value,
+        detailAddress: detailAddressInput.value,
+        phone: phoneInput.value,
+      }),
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("서버 오류");
+        }
+      })
+      .then(data => {
+        alert(data.message);
+        location.replace("/");
+      })
+      .catch(error => {
+        console.error(error);
+        alert("회원가입에 실패했습니다. 다시 시도해주세요.");
+      });
   } else {
     checkValid();
   }
