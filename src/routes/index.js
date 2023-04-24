864b3224aa4fa731d5c868e7d68e9edf13806701
[SW4/1반] 신혜지 <shinhj4068@gmail.com>
2023-04-25 01:54:47 +0900

    feat: category api 구현

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { getCategoryList } = require("../controllers/categoryController.js");
 
 // router.get("/", function (req, res, next) {
 //   res.render("/home/home", { title: "Express" });
@@ -8,5 +9,8 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.render("home/home.html");
 });
+
+// 카테고리 목록 조회
+router.get("/category", getCategoryList);
 
 module.exports = router;
