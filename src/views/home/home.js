280e60cbb40bde1a601330c6da54781eabbfd3b3
[SW4/1반] 신혜지 <shinhj4068@gmail.com>
2023-04-25 15:39:16 +0900

    fix: img href 수정

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -74,7 +74,7 @@ const books = document.querySelector(".books");
 // 책 html 생성
 const createBook = (book) => {
   return `<div class="book">
-            <a class="book-link" href="/product/${book.isbn}">
+            <a class="book-link" href="/book-detail/${book.isbn}">
               <div class="img-container">
                 <img src="${book.image_path}" class="book-img" alt="${book.title}"/>
               </div>
