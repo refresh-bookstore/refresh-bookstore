eb8535a085c3891e363ed2636acef3418d612151
HOJUN SONG <bmp.tom@gmail.com>
2023-04-26 01:38:41 +0900

    update: 유저정보 수정 라우팅 추가

--- src/routes/userRouters.js
+++ src/routes/userRouters.js
@@ -3,17 +3,11 @@ const router = express.Router();
 const { validateUserRegistration } = require("../middlewares/userValidation");
 const loginController = require("../controllers/loginController");
 const checkSession = require("../middlewares/checkSession");
-const {
-  createUser,
-  getUsers,
-  updateUsers,
-} = require("../controllers/userController");
+const { createUser, updateUsers } = require("../controllers/userController");
 const { isAdmin } = require("../middlewares/isAdmin.js");
 
 router.post("/register", validateUserRegistration, createUser);
-
 router.post("/login", loginController.login);
-
-router.put("/", checkSession, isAdmin, updateUsers);
+router.post("/update", checkSession, updateUserInfo);
 
 module.exports = router;
