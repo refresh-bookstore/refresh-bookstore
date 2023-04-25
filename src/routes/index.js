ce5402b746d10fec2339be1cab6c312361f685d0
HOJUN SONG <bmp.tom@gmail.com>
2023-04-25 16:20:11 +0900

    update: applyting pagerouting

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// router.get("/", function (req, res, next) {
-//   res.render("/home/home", { title: "Express" });
-//
-
 router.get("/", (req, res) => {
   res.render("home/home.html");
 });
--- src/routes/orderNcartRouters.js
+++ src/routes/orderNcartRouters.js
@@ -3,7 +3,4 @@ const checkSession = require("../middlewares/checkSession");
 const orderController = require("../controllers/orderController");
 const router = Router();
 
-router.post("/", checkSession, orderController.createOrder);
-router.get("/", orderController.getOrderList);
-
 module.exports = router;
