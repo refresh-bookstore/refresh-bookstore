e8df7912ee944b84eb4a8c24ddad28f06ce79484
HOJUN SONG <bmp.tom@gmail.com>
2023-04-26 02:44:07 +0900

    fix: 엔드포인트 재지정

--- src/routes/userRouters.js
+++ src/routes/userRouters.js
@@ -4,11 +4,13 @@ const { validateUserRegistration } = require("../middlewares/userValidation");
 const { login, logout } = require("../controllers/loginController");
 const checkSession = require("../middlewares/checkSession");
 const { createUser, updateUserInfo } = require("../controllers/userController");
+const { deleteUserByEmail } = require("../services/userService");
 const { isAdmin } = require("../middlewares/isAdmin.js");
 
 router.post("/register", validateUserRegistration, createUser);
 router.post("/login", login);
-router.post("/update", checkSession, updateUserInfo);
-router.post("/logout", checkSession, logout);
+router.post("user-mypage/update", checkSession, updateUserInfo);
+router.post("user-mypage/logout", checkSession, logout);
+router.delete("user-mypage/delete", checkSession, deleteUserByEmail);
 
 module.exports = router;
