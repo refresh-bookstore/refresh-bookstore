ba460c76f10c34f476c8694da19f5b35b6c3fe83
HOJUN SONG <bmp.tom@gmail.com>
2023-04-26 02:32:04 +0900

    update: logout routing

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -30,3 +30,22 @@ exports.login = async (req, res) => {
     });
   }
 };
+
+exports.logout = (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({
+        message:
+          err.message ||
+          "로그아웃 중 문제가 발생했습니다. 잠시 후 다시 시도해주세요.",
+      });
+    } else {
+      res.clearCookie("connect.sid"); // 세션 쿠키 삭제
+      res.json({
+        message: "로그아웃 되었습니다.",
+        redirect: "/login",
+      });
+    }
+  });
+};
