00a075021f099100c3b2fce985843aaea07affb3
baansuk <baansuk_hwang@naver.com>
2023-04-26 01:06:07 +0900

    fix: home html 오류 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getCategoryList } = require("../controllers/categoryController");
-const { getProductList } = require("../controllers/productController");
+//const { getCategoryList } = require("../controllers/categoryController");
+//const { getProductList } = require("../controllers/productController");
 
  router.get("/", (req, res) => {
    res.render("home/home.html");
  });
 
 // 카테고리 목록 조회 - FE 임시 작성
-router.get("/category", getCategoryList);
+//router.get("/category", getCategoryList);
 
 //제품 전체 조회하기 - FE 임시 작성
-router.get("/product", getProductList);
+//router.get("/product", getProductList);
 
 module.exports = router;
