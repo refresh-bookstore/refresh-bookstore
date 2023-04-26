025a90e8ab38db3e43b48e77d0008e8664589553
youngchae <010101chae@gmail.com>
2023-04-26 12:55:07 +0900

    fix: cart 선택삭제 오류 해결

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -20,42 +20,10 @@ const deliveryFee = document.querySelector('.deliveryFee');
 const totalCost = document.querySelector('.totalCost');
 const orderButton = document.querySelector('.order__button');
 
-// 예시 주문데이터
-// const order = [
-//   {
-//     "title":"프론트엔드 성능 최적화 가이드",
-//     "author":"유동균",
-//     "publisher":"인사이트",
-//     "publication_date":"2022-11-14T15:00:00.000Z",
-//     "isbn":"9788966263745",
-//     "description":"'웹 성능 최적화'는 프론트엔드 개발자라면 반드시 고민해야 하는 이슈다. 서비스 환경과 상황에 따라 필요한 최적화 포인트가 다르고, 기법도 매우 다양하기에 개발 중인 서비스 특성에 맞게 커스터마이징하는 능력이 중요하다.\n\n  저자는 수많은 기업의 웹 서비스 성능을 컨설팅하고, 삼성 SSAFY, 프로그래머스, 인프런, 스터디파이 등 강의 플랫폼에서 최적화 및 개발 강의를 진행한 경험을 책에 고스란히 녹여 냈다. '성능 최적화' 주제를 효과적으로 배우는 데 이론보다는 실습이 중요함을 강조하며 현장감 있게 구성했다. 이 책은 실생활에서 흔히 개발하는 4가지 실습 사이트를 예제로, 직관적인 최적화 노하우를 전달한다.",
-//     "price":22000,
-//     "image_path":"../product-images/9788966263745.png",
-//     "category":"프론트엔드",
-//     "amount":1,
-//   }
-// ];
-
-// const bookData = {
-//   "title":"모던 자바스크립트 Deep Dive ",
-//   "author":"이웅모",
-//   "publisher":"위키북스",
-//   "publication_date":"2020-09-24T15:00:00.000Z",
-//   "isbn":"9791158392239",
-//   "description":"자바스크립트를 둘러싼 기본 개념을 정확하고 구체적으로 설명하고, 자바스크립트 코드의 동작 원리를 집요하게 파헤친다. 작성한 코드가 컴퓨터 내부에서 어떻게 동작할 것인지 예측하고, 명확히 설명할 수 있도록 돕는다. 또한 최신 자바스크립트 명세를 반영해 안정적이고 효율적인 코드를 작성할 수 있는 기본기를 다지고, 실전에서 쓰이는 모던 자바스크립트 프레임워크나 도구를 완벽하게 이해하고 활용할 수 있게 도와준다.",
-//   "price":45000,
-//   "image_path":"../product-images/9791158392239.png",
-//   "category":"프론트엔드",
-//   "amount":1,
-// };
-// order.push(bookData);
-// saveToCart(order);
-
-let data = JSON.parse(localStorage.getItem('cart'));
 // 로컬스토리지에서 데이터 불러오기, 체크박스, 수량조절, 삭제버튼 활성화, 가격 출력 자동화
 function renderBooks() {
-  data = JSON.parse(localStorage.getItem('cart'));
-  if (data === null) {
+  const data = JSON.parse(localStorage.getItem('cart'));
+  if (!data || data.length === 0) {
     // 장바구니에 상품이 없습니다
     cart.innerHTML = `<p class="empty-cart">상품이 없습니다.</p>`;
     localStorage.clear();
@@ -144,6 +112,7 @@ function activateAmountBtn() {
   // 수량 증가 클릭 이벤트
   plusBtn.forEach((btn, idx) => {
     btn.addEventListener('click', () => {
+      const data = JSON.parse(localStorage.getItem('cart'));
       btn.previousElementSibling.value = Number(btn.previousElementSibling.value) + 1;
       data[idx].amount += 1;
       localStorage.removeItem('cart');
@@ -159,6 +128,7 @@ function activateAmountBtn() {
       if (Number(btn.nextElementSibling.value) === 1) {
         btn.nextElementSibling.value = 1;
       } else {
+        const data = JSON.parse(localStorage.getItem('cart'));
         btn.nextElementSibling.value = Number(btn.nextElementSibling.value) - 1;
         data[idx].amount -= 1;
         localStorage.removeItem('cart');
@@ -176,6 +146,7 @@ function activateAmountBtn() {
       if (amount.value <= 1 || amount.value === undefined) {
         amount.value = 1;
       } 
+      const data = JSON.parse(localStorage.getItem('cart'));
       data[idx].amount = Number(amount.value);
       localStorage.removeItem('cart');
       saveToCart(data);
@@ -191,8 +162,10 @@ function activateDeleteBtn() {
   const deleteBtn = document.querySelectorAll('.deleteBtn');
   deleteBtn.forEach((btn, idx) => {
     btn.addEventListener('click', () => {
+      const data = JSON.parse(localStorage.getItem('cart'));
       data.splice(idx, 1);
       localStorage.removeItem('cart');
+      cart.innerHTML = '';
       saveToCart(data);
       renderBooks();
     });
@@ -211,6 +184,7 @@ function activateDeleteSelectedBtn() {
       });
     }
     if (checkedIndex.length) {
+      const data = JSON.parse(localStorage.getItem('cart'));
       checkedIndex.forEach(indexNum => {
         data.splice(indexNum, 1);
       });
